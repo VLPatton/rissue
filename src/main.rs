ceece99bab0ebc6fc1c2d5c32f88af62f15b94b5
Ayzee Patton <vlpatton@vlpatton.gay>
2024-02-03 00:21:10 -0600

    Apply a real person layout.
    There's now some structure to the DOM. Still needs color and fonts tho.
    Idk if I'll actually bother tho tbh.

--- src/main.rs
+++ src/main.rs
@@ -153,7 +153,9 @@ async fn new_issue(web::Form(body): web::Form<CreateIssueRequest>) -> impl Respo
 async fn issue_number(number: web::Path<u64>) -> impl Responder {
     let conn = data::get_connection().lock().unwrap();
 
-    let mut html = String::from("<div>");
+
+    let mut html = String::new();
+    html.push_str("<div><button hx-get=\"/issues\" hx-target=\"#latest-div\" hx-swap=\"innerHTML\" class=\"create-button\">Go home</button></div>\n<div>");
 
     conn.iterate(std::format!("SELECT * FROM issuetab WHERE issue_id={number};"), |pairs| {
         for &(name, value) in pairs.iter() {
@@ -209,7 +211,7 @@ async fn issue_number(number: web::Path<u64>) -> impl Responder {
         true
     }).unwrap();
 
-    html.push_str(&std::format!("<button hx-get=\"/new/comment\" hx-swap=\"outerHTML\">Post new comment</button>"));
+    html.push_str(&std::format!("<button hx-get=\"/new/comment\" hx-swap=\"outerHTML\" class=\"create-button\">Post new comment</button>\n"));
 
     HttpResponse::Ok().content_type(ContentType::html()).body(html)
 }
@@ -219,7 +221,7 @@ async fn index_file(filename: web::Path<String>) -> impl Responder {
     let content = fs::read_to_string(filename.to_string());
 
     if let Ok(content) = content {
-        HttpResponse::Ok().content_type(if filename.to_string().ends_with(".js") { mime::TEXT_JAVASCRIPT } else { mime::TEXT_PLAIN_UTF_8 }).body(content)
+        HttpResponse::Ok().content_type(if filename.to_string().ends_with(".js") { mime::TEXT_JAVASCRIPT } else { mime::TEXT_CSS }).body(content)
     } else {
         HttpResponse::InternalServerError().content_type(ContentType::html()).body("500 - INTERNAL SERVER ERROR")
     }
@@ -254,6 +256,31 @@ async fn index() -> impl Responder {
     HttpResponse::Ok().content_type(ContentType::html()).body(html)
 }
 
+#[get("/issues")]
+async fn issues() -> impl Responder {
+    let mut builder = String::new();
+
+    let conn = data::get_connection().lock().unwrap();
+    conn.iterate("SELECT * FROM issuetab WHERE post_date>DATETIME('now', '-1 month');", |pairs| {
+        let mut id = "";
+        let mut title = "";
+        for &(name, value) in pairs.iter() {
+            if name == "issue_id" {
+                id = value.unwrap_or("");
+            }
+            if name == "title" {
+                title = value.unwrap_or("");
+            }
+        }
+        builder = builder.clone() + &std::format!("<div class=\"issue-title\" hx-get=\"/issue/{id}\" hx-swap=\"innerHTML\" hx-target=\"#latest-div\">{id} - {title}</div>\n");
+        true
+    }).unwrap();
+
+    println!("{builder}");
+
+    HttpResponse::Ok().content_type(ContentType::html()).body(builder)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     data::init_db_unchecked();
@@ -261,6 +288,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(
             || App::new()
                 .service(issue_number)
+                .service(issues)
                 .service(index_file)
                 .service(new_comment_prompt)
                 .service(new_comment)
